ac3214c0cc8397531373b97a0a88ab1d96076d3a
Daniel Brodsky <broddanik@gmail.com>
2023-07-29 14:49:47 +0300

    macro.h and macro.h

--- macro.c
+++ macro.c
@@ -1,5 +1,27 @@
-#include "macro.h"
+/*********************************FILE__HEADER*********************************\
+*
+* File:                 macro.c
+* Authors:              Daniel Brodsky & Lior Katav
+* Date:                 August-2023
+* Description:          This file contains implementations of functions used to
+*                       create, manipulate, and delete macros in the assembly
+*                       language program.
+*                       Macros are stored in a dynamic array (vector).
+\******************************************************************************/
 
+/******************************** Header Files ********************************/
+#include "macro.h"
+#include "utils.h"  /* my_strdup function */
+
+/************************* Functions  Implementations *************************/
+/**
+ * Creates a new MacroVector, allocates initial memory, and sets default values.
+ *
+ * @return - A pointer to the newly created MacroVector.
+ *
+ * The function allocates memory for a MacroVector struct and an array of Macro pointers,
+ * sets the initial capacity and size, and returns a pointer to the new MacroVector.
+ */
 MacroVector *new_macro_vector() {
     MacroVector *mv = malloc(sizeof(MacroVector));
     mv->capacity = INITIAL_CAPACITY;
@@ -7,7 +29,17 @@ MacroVector *new_macro_vector() {
     mv->macros = malloc(sizeof(Macro *) * mv->capacity);
     return mv;
 }
-
+/******************************************************************************/
+/**
+ * Adds a new Macro to the end of a MacroVector.
+ *
+ * @param mv - A pointer to the MacroVector.
+ * @param m - A pointer to the Macro to be added.
+ *
+ * If the MacroVector's current size has reached its capacity,
+ * the function doubles the capacity by reallocating memory.
+ * The Macro is then added to the end of the MacroVector.
+ */
 void push_back_macro(MacroVector *mv, Macro *m) {
     if (mv->size == mv->capacity) {
         mv->capacity *= 2;
@@ -15,17 +47,37 @@ void push_back_macro(MacroVector *mv, Macro *m) {
     }
     mv->macros[mv->size++] = m;
 }
-
+/******************************************************************************/
+/**
+ * Creates a new Macro with the given name.
+ *
+ * @param name - The name to be assigned to the new Macro.
+ * @return - A pointer to the new Macro.
+ *
+ * The function allocates memory for a Macro struct,
+ * duplicates the provided name string and assigns it to the Macro,
+ * initializes a new command vector for the Macro,
+ * and returns a pointer to the new Macro.
+ */
 Macro *new_macro(char *name) {
     Macro *m = malloc(sizeof(Macro));
     m->name = my_strdup(name);
     m->commands = new_vector();
     return m;
 }
-
+/******************************************************************************/
+/**
+ * Frees all memory associated with a Macro, including its name and commands.
+ *
+ * @param m - A pointer to the Macro to be freed.
+ *
+ * The function frees the memory allocated for the Macro's name string,
+ * then frees the command vector associated with the Macro,
+ * and finally frees the memory allocated for the Macro struct itself.
+ */
 void free_macro(Macro *m) {
     free(m->name);
     free_vector(m->commands);
     free(m);
 }
-
+/******************************************************************************/
--- macro.h
+++ macro.h
@@ -1,31 +1,55 @@
 /*********************************FILE__HEADER*********************************\
 * File:					macro.h
-* Author:				Daniel Brodsky			 		  												  								
-* Date:					July-2023			   								
-* Description:			
+* Author:				Daniel Brodsky & Lior Katav
+* Date:					August-2023
+* Description:			This header file contains the definitions for the Macro
+*                       and MacroVector structs and their related functions.
+*                       These are used for handling and manipulating macros
+*                       in assembly code processing.
 \******************************************************************************/
 
 #ifndef MACRO_H
 #define MACRO_H
 
-#include "vector.h"
-#include "utils.h" /* my_strdup */
+#include "vector.h" /* for Vector struct */
 
+/**************************** Structs  Definitions ****************************/
+/* A struct representing a Macro. It has a name and a list of commands. */
 typedef struct {
     char *name;
     Vector *commands;
 } Macro;
 
+/** A struct representing a MacroVector.
+ * It contains an array of Macro pointers and size information. */
 typedef struct {
     Macro **macros;
     int size;
     int capacity;
 } MacroVector;
-
+/******************************************************************************/
+/**
+ * Creates a new MacroVector and allocates initial memory.
+ * @return A pointer to the newly created MacroVector.
+ */
 MacroVector *new_macro_vector();
+/**
+ * Adds a new Macro to a MacroVector.
+ * @param mv - A pointer to the MacroVector.
+ * @param m - A pointer to the Macro to be added.
+ */
 void push_back_macro(MacroVector *mv, Macro *m);
+/**
+ * Creates a new Macro.
+ * @param name - The name to be assigned to the new Macro.
+ * @return A pointer to the new Macro.
+ */
 Macro *new_macro(char *name);
+/**
+ * Frees all memory associated with a Macro.
+ * @param m - A pointer to the Macro to be freed.
+ */
 void free_macro(Macro *m);
-
+/******************************************************************************/
 #endif
 
